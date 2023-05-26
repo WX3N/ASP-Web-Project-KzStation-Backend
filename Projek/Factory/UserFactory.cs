bf1882380bcb2ed4430dc98e3dbe6bfe91ed76a4
WX3N <wx3n24@gmail.com>
2023-05-26 15:15:43 +0700

    Update 2

---
+++ Projek/Factory/UserFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Projek.Factory;
+
+namespace Projek.Factory
+{
+    public class UserFactory
+    {
+        public static User createUser(string name, string email, string gender, string address, string password, string role)
+        {
+            return new User
+            {
+                Email = email,
+                Name = name,
+                Gender = gender,
+                Address = address,
+                Password = password,
+                Role = "User"
+            };
+        }
+    }
+}
---
+++ Projek/Repository/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Projek.Model;
+using Projek.Factory;
+
+namespace Projek.Repository
+{
+    public class UserRepository
+    {
+        Database1Entities1 db = new Database1Entities1();
+        public void Register(string name, string email, string gender, string address, string password, string role)
+        {
+            Customer customer = UserFactory.createUser(name, email, gender, address, password, role);
+
+            db.Customers.Add(customer);
+
+            db.SaveChanges();
+        }
+
+        public User Login(string email, string password)
+        {
+            User user = db.Users.Where(x => x.UserEmail == email && x.Password == password).FirstOrDefault();
+            return user;
+        }
+
+        public User getUser(int id)
+        {
+            User user = db.Users.Where(x => x.Id == id).FirstOrDefault();
+            return user;
+        }
+    }
+}
